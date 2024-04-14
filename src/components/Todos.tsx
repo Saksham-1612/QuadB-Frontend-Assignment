cbd2fe30b21dc2711ffca59fc34ded66319ca576
Saksham Srivastava <saksham16dec@gmail.com>
2024-04-14 21:06:14 +0530

    fixed responsiveness

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -7,7 +7,7 @@ import { removeTodo, updateTodo } from "../../redux/slices/todo";
 import AddTodo from "./AddTodo";
 import { RxCross1 } from "react-icons/rx";
 
-interface todoType {
+interface TodoType {
   id: number;
   text: string;
   isDone: boolean;
@@ -20,7 +20,7 @@ const Todos = () => {
   const [editingTodoId, setEditingTodoId] = useState<number | null>(null);
   const [editedText, setEditedText] = useState("");
 
-  const markDone = (todo: todoType) => {
+  const markDone = (todo: TodoType) => {
     dispatch(
       updateTodo({
         id: todo.id,
@@ -29,12 +29,12 @@ const Todos = () => {
     );
   };
 
-  const handleEdit = (todo: todoType) => {
+  const handleEdit = (todo: TodoType) => {
     setEditingTodoId(todo.id);
     setEditedText(todo.text);
   };
 
-  const handleSaveEdit = (todo: todoType) => {
+  const handleSaveEdit = (todo: TodoType) => {
     dispatch(
       updateTodo({
         id: todo.id,
@@ -46,7 +46,7 @@ const Todos = () => {
 
   const handleKeyPress = (
     event: React.KeyboardEvent<HTMLInputElement>,
-    todo: todoType
+    todo: TodoType
   ) => {
     if (event.key === "Enter") {
       handleSaveEdit(todo);
@@ -57,46 +57,50 @@ const Todos = () => {
     <div className="min-h-screen flex flex-col items-center w-screen">
       <h2 className="my-2 text-3xl text-orange-500 font-semibold">TASK TOP</h2>
       <AddTodo />
-      <div>
-        {todos.map((todo: todoType) => (
+      <div className="w-full max-w-screen-lg">
+        {todos.map((todo: TodoType) => (
           <div
-            className="flex space-x-5 text-xl my-4  text-black rounded-md bg-blue-200"
+            className={`flex flex-col md:flex-row items-center justify-between space-y-2 md:space-y-0 md:space-x-5 text-xl my-4  text-black rounded-md bg-blue-200 ${
+              editingTodoId === todo.id ? "" : ""
+            }`}
             key={todo.id}
           >
-            <li className="flex space-x-3 min-w-[50vw]">
-              <div className="px-4 py-2 min-[20vw]:">
+            <li className="flex space-x-3 w-full">
+              <div className="w-full md:w-1/2 px-1 py-2">
                 {editingTodoId === todo.id ? (
                   <input
                     type="text"
                     value={editedText}
                     onChange={(e) => setEditedText(e.target.value)}
                     onKeyDown={(e) => handleKeyPress(e, todo)}
-                    className="bg-blue-200 rounded border text-base outline-none text-gray-800 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    className="w-full bg-blue-200 rounded border text-base outline-none text-gray-800 py-1  leading-8 transition-colors duration-200 ease-in-out"
                   />
                 ) : (
-                  <p>{todo.text}</p>
+                  <p className="w-full break-all">{todo.text}</p>
                 )}
               </div>
               <div className="px-4 py-2 flex items-center">
                 {todo.isDone ? <p>Done</p> : <p>Not Done</p>}
               </div>
             </li>
-            <button onClick={() => markDone(todo)}>
-              {todo.isDone ? (
-                <IoMdCheckmark className="text-green-500" />
-              ) : (
-                <RxCross1 className="text-red-500" />
-              )}
-            </button>
-            <button onClick={() => handleEdit(todo)}>
-              <FaPencil />
-            </button>
-            <button
-              className="mr-2"
-              onClick={() => dispatch(removeTodo({ id: todo.id }))}
-            >
-              <MdDelete className="text-red-500" />
-            </button>
+            <div className="flex space-x-2">
+              <button onClick={() => markDone(todo)}>
+                {todo.isDone ? (
+                  <IoMdCheckmark className="text-green-500" />
+                ) : (
+                  <RxCross1 className="text-red-500" />
+                )}
+              </button>
+              <button onClick={() => handleEdit(todo)}>
+                <FaPencil />
+              </button>
+              <button
+                className="mr-2"
+                onClick={() => dispatch(removeTodo({ id: todo.id }))}
+              >
+                <MdDelete className="text-red-500" />
+              </button>
+            </div>
           </div>
         ))}
       </div>
